a49197ed1608636d40c480af021562f45570977b
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-04-02 12:19:39 -0600

    C programming language foundations

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+   float a, b, c, sum;
+
+   printf("Input three floats:");
+
+   scanf("%f%f%f", &a, &b, &c);
+
+   printf("a = %f, b = %f, c = %f\n", a, b, c);
+
+   sum = a + b + c;
+
+   printf("sum = %f\n\n", sum);
+   return 0;
+}
